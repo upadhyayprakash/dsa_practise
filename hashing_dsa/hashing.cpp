49242d62c1bcf08dc5cb9fdf8e1cb190901420fc
Prakash Upadhyay <prakash.upadhyay93@yahoo.com>
2024-07-04 13:12:16 +0530

    feat: freq counter using map

--- hashing_dsa/hashing.cpp
+++ hashing_dsa/hashing.cpp
@@ -1,7 +1,8 @@
+#include <map>
 #include <iostream>
 using namespace std;
 
-void basic_hash() {
+void basic_hash_counter_int() {
     // Take size of array
     int size;
     cin >> size;
@@ -11,9 +12,9 @@ void basic_hash() {
         cout << arr[i] << " ";
     }
     
-    cout << endl;    
+    cout << endl;
 
-    // Creatig hash array
+    // Creating hash array
     int hash[5] = {0};
     for(int i = 0; i < size; i++) {
         hash[arr[i]] += 1;
@@ -30,7 +31,69 @@ void basic_hash() {
     }
 }
 
+void basic_hash_counter_char() {
+    // Input string sequence
+    string str = "abcdabcaa"; // all small-case alphabets ONLY
+
+    // Hash into a frequency array
+    int freqArr[26] = {0};
+    for(int i = 0; i < str.length(); i++) {
+        freqArr[str[i] - 'a'] += 1;
+    }
+
+    // Print freqArr
+    cout << endl << "Frequency Array:" << endl;
+    for(auto i: freqArr) {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    // query array
+    char qArr[] = {'c', 'a', 'd', 'b'};
+    int qSize = sizeof(qArr) / sizeof(*qArr);
+    for(int i = 0; i < qSize; i++) {
+        cout << "Count of " << qArr[i] << ": " << freqArr[qArr[i] - 'a'] << endl;
+    }
+}
+
+void map_counter_int() { // For integers
+    // Take size of array
+    int size;
+    cin >> size;
+    int arr[size];
+    map<int, int> freqArr; // frequency array declaration
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+        freqArr[arr[i]]++;
+        cout << arr[i] << " "; // display input array elements
+    }
+    
+    cout << endl;
+
+    // Print the frequency Map
+    cout << "Map: " << endl;
+    for(auto i: freqArr) {
+        cout << i.first << "->" << i.second << " " << endl;
+    }
+    
+    cout << endl;
+
+    // Take size of queries
+    int qSize;
+    cin >> qSize;
+    
+    while(qSize--) {
+        int qNum;
+        cin >> qNum;
+        cout << "Count of " << qNum << ": " << freqArr[qNum] << endl;
+    }
+
+}
+
 int main() {
-    basic_hash();
+    // basic_hash_counter_int();
+    basic_hash_counter_char();
+    // map_counter_int();
     return 0;
 }
--- hashing_dsa/hashing_notes.md
+++ hashing_dsa/hashing_notes.md
@@ -1,3 +1,13 @@
 # Hashing
 Striver's Video (Basic): https://www.youtube.com/watch?v=KEs5UyBJ39g
 
+- An frequency array can be used as a counter for input arr.
+- The size of freqArr can be determined from the maxEle of input array.
+    freqArr[maxEle+1]
+- For character hashing, we can rely on array[255+1] as there're only 255 ASCII characters.
+- But for integers, size of `freqArr[size]` is limited, hence for a huge `maxEle`, it's not possible to create a freqArr. Eg. 10^7 or more. Hence array's are not suitable for all counting frequency in huge range of integers.
+- Hence, we use std::map from C++ STL library. Which can store <key, value> pair which are sorted by default.
+
+## Map
+- Map stores key values pair in sorted order of `key`.
+- 
