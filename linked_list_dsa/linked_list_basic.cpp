00415a01a6099a4ae3ac9ed5330214b8f3ea4cde
Prakash Upadhyay <prakash.upadhyay93@yahoo.com>
2024-07-08 16:36:30 +0530

    feat: linked list insertion

--- linked_list_dsa/linked_list_basic.cpp
+++ linked_list_dsa/linked_list_basic.cpp
@@ -50,6 +50,7 @@ int get_length_linked_list(Node* head) {
         count++;
         temp = temp->next;
     }
+    cout << "\nLength: " << count << endl;
     return count;
 }
 
@@ -149,6 +150,82 @@ Node* delete_by_value(Node* head, int value) {
     return head;
 }
 
+Node* insert_head(Node* head, int ele) {
+    Node* newNode = new Node(ele);
+
+    Node* temp = head;
+    newNode->next = temp;
+    head = newNode;
+    return head;
+}
+
+Node* insert_tail(Node* head, int ele) {
+    Node* newNode = new Node(ele);
+
+    if(head == nullptr) {
+        head = newNode;
+        return head;
+    }
+
+    Node* temp = head;
+    while(temp->next) {
+        temp = temp->next;
+    }
+
+    temp->next = newNode;
+    return head;
+}
+
+Node* insert_position(Node* head, int ele, int pos) {
+    if(pos < 1 || pos > (get_length_linked_list(head) + 1)) {
+        cout << "\nCan't insert at given position: " << pos << endl;
+        return head;
+    }
+    Node* newNode = new Node(ele);
+
+    if(pos == 1 || head == nullptr) {
+        Node* temp = head;
+        newNode->next = temp;
+        head = newNode;
+        return head;
+    }
+
+    int count = 1;
+    Node* temp = head;
+    while(temp) {
+        count++;
+        if(count == pos) {
+            newNode->next = temp->next;
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
+Node* insert_before_value(Node* head, int ele, int value) {
+    if(head == nullptr) { // LL is empty
+        return head;
+    }
+    if(head->data == value) { // target 'value' is at the 'head'
+        return new Node(ele, head);
+    }
+
+    Node* temp = head;
+    while(temp->next) {
+        if(temp->next->data == value) {
+            // insert after temp
+            temp->next = new Node(ele, temp->next); // constructor with next pointer
+            break;
+        }
+        temp = temp->next;
+    }
+
+    return head;
+}
+
 int main() {
     vector<int> arr = {};
     Node* head = arrToLL(arr);
@@ -177,20 +254,41 @@ int main() {
     head = delete_tail(head);
     print_linked_list(head);
 
-    int pos = 4;
+    int pos = 2;
     cout << "\nDelete at Position " << pos << ":\n";
     head = delete_position(head, pos);
     print_linked_list(head);
     
     
-    int value = 8;
+    int value = 5;
     cout << "\nDelete by value " << value << ":\n";
     head = delete_by_value(head, value);
     print_linked_list(head);
     
-
-
     /* Insertion in LinkedList */
+    cout << "\nInsert before head:\n";
+    int ele1 = 12;
+    head = insert_head(head, ele1);
+    print_linked_list(head);
+
+    cout << "\nInsert after tail:\n";
+    int ele2 = 98;
+    head = insert_tail(head, ele2);
+    print_linked_list(head);
+
+    int pos3 = 3;
+    int ele3 = 55;
+    cout << "\nInsert " << ele3 << " at Position " << pos3 << ":\n";
+    head = insert_position(head, ele3, pos3);
+    print_linked_list(head);
+    
+    
+    int newEle4 = 54;
+    int value4 = 55;
+    cout << "\nInsert before the given value: " << value << ":\n";
+    head = insert_before_value(head, newEle4, value4);
+    print_linked_list(head);
+    
 
     return 0;
 }
