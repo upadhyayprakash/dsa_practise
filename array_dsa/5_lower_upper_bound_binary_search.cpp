104999c5f370cdb59a052c825577ba4679be93a9
Prakash Upadhyay <prakash.upadhyay93@yahoo.com>
2024-07-23 23:22:55 +0530

    feat: first and last occurence of a number binary search

--- array_dsa/5_lower_upper_bound_binary_search.cpp
+++ array_dsa/5_lower_upper_bound_binary_search.cpp
@@ -55,7 +55,7 @@ int get_ceil(vector<int> arr, int n, int target) {
  * - It's similar to finding 'lower-bound' for a 'target' ele in sorted array.
  * - Same algo as lower-bound
  */
-int find_insert_position(vector<int> arr, int n, int target) { // Time Complexity: O(log2N)
+int find_insert_position(vector<int> &arr, int n, int target) { // Time Complexity: O(log2N)
     int ans = n;
     int low = 0;
     int high = n-1;
@@ -78,7 +78,7 @@ int find_insert_position(vector<int> arr, int n, int target) { // Time Complexit
  * A upper bound means an element that's > target. We'll return the index of that 'ele'
  * or a pseudo index 'n' if no such lower bound is possible.
  */
-int get_upper_bound_index(vector<int> arr, int n, int target) { // Time Complexity: O(log2N)
+int get_upper_bound_index(vector<int> &arr, int n, int target) { // Time Complexity: O(log2N)
     int ans = n;
     int low = 0;
     int high = n-1;
@@ -100,7 +100,7 @@ int get_upper_bound_index(vector<int> arr, int n, int target) { // Time Complexi
  * A lower bound means an element that's >= target. We'll return the index of that 'ele'
  * or a pseudo index 'n' if no such lower bound is possible.
  */
-int get_lower_bound_index(vector<int> arr, int n, int target) { // Time Complexity: O(log2N)
+int get_lower_bound_index(vector<int> &arr, int n, int target) { // Time Complexity: O(log2N)
     int ans = n;
     int low = 0;
     int high = n-1;
@@ -118,10 +118,22 @@ int get_lower_bound_index(vector<int> arr, int n, int target) { // Time Complexi
     return ans;
 }
 
+/**
+ * Find 'first' and 'last' occurence of the given number in an array.
+ */
+pair<int, int> get_first_last_occurence(vector<int> &arr, int n, int target) { // Time Complexity: O(logN)
+    int lb = get_lower_bound_index(arr, n, target);
+    if(lb == n || arr[lb] != target)
+        return {-1, -1};
+    
+    int ub = get_upper_bound_index(arr, n, target);
+    return {lb, ub - 1};
+}
+
 int main() {
     vector<int> arr = {1,2,3,4,5,7,7,8,9,10};
     int n = arr.size();
-    int target = 6;
+    int target = 7;
     cout << "Lower Bound of " << target << ":\n";
     cout << get_lower_bound_index(arr, n, target);
 
@@ -136,6 +148,10 @@ int main() {
 
     cout << "\n\nFloor of " << target << ": \n";
     cout << get_floor(arr, n, target);
+
+    cout << "\n\n1st and last index of " << target << ":\n";
+    pair<int, int> result = get_first_last_occurence(arr, n, target);
+    cout << "{" << result.first << ", " << result.second << "}";
 
     return 0;
 }
