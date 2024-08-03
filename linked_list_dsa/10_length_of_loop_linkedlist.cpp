e9f83ccce7c9d12532b3bd112c459efa05d244fb
Prakash Upadhyay <prakash.upadhyay93@yahoo.com>
2024-08-03 09:29:58 +0530

    feat: length of loop in linked list

---
+++ linked_list_dsa/10_length_of_loop_linkedlist.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include "utility.h"
+
+using namespace std;
+
+Node* add_a_loop(Node* head) {
+    Node* temp = head;
+    Node* memory = nullptr;
+    int pos = 3;
+    while(temp->next) {
+        temp = temp->next;
+        pos--;
+        if(pos == 1) memory = temp;
+    }
+    temp->next = memory;
+    cout << "\nAdded loop from end to node:\n" << memory->data << endl;
+    return head;
+}
+
+/**
+ * Approach:
+ * 1. Find loop point and keep one pointer at a collision point.
+ * 2. Move the other pointer 1-step with a counter until it meets previous pointer.
+ */
+
+int get_loop_length(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while(fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast) {
+            slow = slow->next;
+            int count = 1;
+            while(slow != fast) {
+                slow = slow->next;
+                count++;
+            }
+            return count;
+        }
+    }
+    return 0;
+}
+
+
+int main() {
+    vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+    Node* head = arrToLL(arr);
+    printLL(head); // IMPORTANT: Don't print after adding loop.
+    head = add_a_loop(head);
+
+    cout << "\nLength of Loop:\n" << get_loop_length(head);
+
+    return 0;
+}
